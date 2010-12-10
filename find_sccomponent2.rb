bccb87ca2423f77b63f51260ff24059e9859084a
Justin Cheng <jccccf@gmail.com>
2010-12-10 01:39:47 -0500

    Fixed SCComponent

--- find_sccomponent2.rb
+++ find_sccomponent2.rb
@@ -2,6 +2,36 @@ require File.join(File.dirname(__FILE__), 'constants')
 require File.join(File.dirname(__FILE__), 'citer_functions')
 require File.join(File.dirname(__FILE__), 'adj_graph')
 
+class FindStronglyConnected
+  def initialize(citers)
+    @citers = citers
+    @running_count = 0
+    @citer_graph = AdjGraph.new
+    @seen = Hash.new
+  end
+  
+  def iterate(curr)
+    @seen[curr] = true
+    
+    @seen.each_key do |k|
+      if @citers[curr] && @citers[curr][k]
+        @citer_graph.add_directed_edge(curr,k)
+        #puts "adding #{curr} #{k}"
+        @citers[curr].delete k
+      end
+      if @citers[k] && @citers[k][curr]
+        @citer_graph.add_directed_edge(k,curr)
+        #puts "adding #{k} #{curr}"
+        @citers[k].delete curr
+      end
+    end
+    
+    @running_count += 1
+    
+    return @running_count, @citer_graph.tarjan_string_limit(100)
+  end
+end
+
 if __FILE__ == $0
   
   Dir.mkdir(Constants::SC_dir) unless File.directory? Constants::SC_dir
@@ -23,37 +53,25 @@ if __FILE__ == $0
       cf.build_directed_citer_graph
       citers = cf.citers
       
+      #puts citers.inspect
+      
       # Add people to the graph and calculate the strongly connected component each time
-      running_count = 0
-      citer_graph = AdjGraph.new
-      seen = Hash.new
-      ofile = File.new(output_file+"~", "w")
-      file = File.new(citers_file, "r")
-      while line = file.gets
-        parts = line.split(' ')
-        curr = parts[0].to_i
-        seen[curr] = true
-
-        citers.each_key do |k|
-          if seen[k] != nil
-            citers[k].each_key do |m|
-              if seen[m] != nil
-                citer_graph.add_directed_edge(k,m)
-                citers[k].delete m
-              end
+      sc = FindStronglyConnected.new(citers)
+      File.open(output_file+"~","w") do |ofile|
+        File.open(citers_file, "r") do |file|      
+          while line = file.gets
+            curr = line.split[0].to_i
+            
+            rc, ta = sc.iterate(curr)
+            
+            ofile.puts "%d %s" % [rc, ta]
+            print "."
+            if rc >= Constants::CI_limit
+              break
             end
           end
         end
-        
-        running_count += 1
-        ofile.puts "%d %s" % [running_count, citer_graph.tarjan_string_limit(100)]
-        print "."
-        if running_count >= Constants::CI_limit
-          break
-        end
       end
-      file.close
-      ofile.close
       File.rename(output_file+"~",output_file)
       print "\n\n"
     end
